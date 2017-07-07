691c37fddd86667f0f4c24c3964d0d2e2fdb206c
AbdalaGabriel <g.abdala.04@gmail.com>
2017-07-07 15:24:23 -0300

    v 0.5.4 ++
    Fixtures for production

--- public/js/pages/ajax-admin.js
+++ public/js/pages/ajax-admin.js
@@ -26,12 +26,12 @@ function carga()
 			if(value.visible_in_menu == 1)
 			{
 				if(value.can_be_deleted == 0){
-					tablaDatos.append('<tr><td>'+value.title+'<br/><a href="#" class="quickEdit" data-toggle="modal" data-target="#quickedit-modal" data-id="'+value.id+'">Quick Edit</a></td> <td>'+value.urlfriendly+'</td><td><a href="http://localhost:8000/admin/paginas/en/'+value.id+'/edit" data-id="'+value.id+'">Versión en inglés</a></td><td><a href="'+baseurl+'admin/paginas/'+value.id+'/edit" data-id="'+value.id+'">Editar</a></td></tr>');
+					tablaDatos.append('<tr><td>'+value.title+'<br/><a href="#" class="quickEdit" data-toggle="modal" data-target="#quickedit-modal" data-id="'+value.id+'">Quick Edit</a></td> <td>'+value.urlfriendly+'</td><td><a href="/admin/paginas/en/'+value.id+'/edit" data-id="'+value.id+'">Versión en inglés</a></td><td><a href="'+baseurl+'admin/paginas/'+value.id+'/edit" data-id="'+value.id+'">Editar</a></td><td>-</td></tr>');
 		
 				}
 				else
 				{
-					tablaDatos.append('<tr><td>'+value.title+'<br/><a href="#" class="quickEdit" data-toggle="modal" data-target="#quickedit-modal" data-id="'+value.id+'">Quick Edit</a></td> <td>'+value.urlfriendly+'</td><td><a href="http://localhost:8000/admin/paginas/en/'+value.id+'/edit" data-id="'+value.id+'">Versión en inglés</a></td><td><a href="'+baseurl+'admin/paginas/'+value.id+'/edit" data-id="'+value.id+'">Editar</a></td><td><a href="#" class="delete" data-toggle="modal" data-target="#myModal" data-id="'+value.id+'">Eliminar</button></td></tr>');
+					tablaDatos.append('<tr><td>'+value.title+'<br/><a href="#" class="quickEdit" data-toggle="modal" data-target="#quickedit-modal" data-id="'+value.id+'">Quick Edit</a></td> <td>'+value.urlfriendly+'</td><td><a href="/admin/paginas/en/'+value.id+'/edit" data-id="'+value.id+'">Versión en inglés</a></td><td><a href="'+baseurl+'admin/paginas/'+value.id+'/edit" data-id="'+value.id+'">Editar</a></td><td><a href="#" class="delete" data-toggle="modal" data-target="#myModal" data-id="'+value.id+'">Eliminar</button></td></tr>');
 				}
 			}
 		});
