4a860175672d7fe6bdedbd7d7ab500ec81e1e480
AbdalaGabriel <g.abdala.04@gmail.com>
2017-07-18 10:14:35 -0300

    v 0.5.9 ++
    Favicon

--- resources/views/front/structure/head.blade.php
+++ resources/views/front/structure/head.blade.php
@@ -10,6 +10,15 @@
 	<!--  Page title -->
 	<title>@yield('mainTitle')</title>
 
+	<!--     Favico    -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="theme-color" content="#000000">
+
+
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<script src="https://apis.google.com/js/platform.js" async defer>
 
@@ -21,27 +30,7 @@
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
-	<!--     Favico    -->
-	<link rel="apple-touch-icon" sizes="57x57" href="/icon.ico/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/icon.ico/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/icon.ico/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/icon.ico/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="/icon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
-	<link rel="shortcut icon" href="/icon.ico/favicon.ico" type="image/x-icon" />
-	<link rel="icon" href="/icon.ico/favicon.ico" type="image/x-icon">
-	<link rel="manifest" href="/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
-
+	
 	<!-- CSS Files -->
 	{!!Html::style('bootstrap-template-assets/materialKit/assets/css/bootstrap.min.css')!!}
 	{!!Html::style('bootstrap-template-assets/materialKit/assets/css/material-kit.css')!!}
